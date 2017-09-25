d0d968076ab3e684801883fefd53bc1472ae5d86
pythongiant <srihari.unnikrishnan@gmail.com>
2017-09-25 15:47:35 +0530

    adding more bricks for our ML wall

--- Review/Review/WriteReview/forms.py
+++ Review/Review/WriteReview/forms.py
@@ -4,5 +4,5 @@ from django import forms
 
 class Review(forms.Form):
     Title=forms.CharField(label="Give a Title",max_length=200,initial="add your title")
-    Review=forms.CharField(label="Your Review",max_length=100000,initial="Type your Review",widget=forms.Textarea)
+    Review=forms.CharField(label="Your Article",max_length=100000,initial="Type your Review",widget=forms.Textarea)
     Tags=forms.CharField(label="add a tag. (Seperate with a ',') - eg. movie,event",max_length=5000,initial=" ")
--- Review/Review/WriteReview/urls.py
+++ Review/Review/WriteReview/urls.py
@@ -7,5 +7,7 @@ from django.conf import settings
 from . import views
 urlpatterns=[
     url(r'^$',views.start,name="Index"),
-    url(r'^review',views.RevDone,name="RevForm")
+    url(r'^review',views.RevDone,name="RevForm"),
+    
+
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
