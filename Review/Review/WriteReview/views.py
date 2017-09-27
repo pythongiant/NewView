7dea3835bf54045ea4c04a9d5d9ec126943386da
pythongiant <srihari.unnikrishnan@gmail.com>
2017-09-27 12:00:23 +0530

    added styles,boi is this going to be coool

--- Review/Review/WriteReview/views.py
+++ Review/Review/WriteReview/views.py
@@ -10,32 +10,11 @@ from . import models
 username:srihari
 password:pass1234
 """
-
+recommendation=[]
 def start(request):
     form = forms.Review()    
     article=models.Reviews.objects.all()
-    art_tags=[]
-    art_tags_all=[]
-    art_tags_all_refined=[]
-    for i in article:
-        art_tags.append(i.Tag)
-    for i in art_tags:
-        string=i.split(',')
-        art_tags_all.append(string)
-    """
-    for i in art_tags_all:
-        for y in i:
-            art_tags_all_refined.append(y)
-    """
-    recommend=0
     
-    #for i in range(len(article)):
-        #tag=art_tags_all[i]
-        #print(simscore(tag,art_tags_all[1]))
-       # recommend=i
-        #recommendation=article.filter(pk=13)
-       # print(recommendation)
-
     context={"form":form,"article":article}
     return render(request,"WriteReview/index.html",context)
    
@@ -59,11 +38,41 @@ def simscore(tag,test):
                 score+=1
                 
     return (score)
+def recommend():
+    global recommendation
+    article=models.Reviews.objects.all()
+    art_tags=[]
+    art_tags_all=[]
+    art_tags_all_refined=[]
+    for i in article:
+        art_tags.append(i.Tag)
+    for i in art_tags:
+        string=i.split(',')
+        art_tags_all.append(string)
+    """
+    for i in art_tags_all:
+        for y in i:
+            art_tags_all_refined.append(y)
+    """
+    recommend=0
+    print(art_tags_all)
+    for i in range(len(article)):
+        tag=art_tags_all[i]
+        sim=simscore(tag,art_tags_all[0])
+        if sim>0:
+            recommend=i
+            recommendation.append(recommend)
+        
+
+
+
 def ReviewDetail(request,rev_id):
     print(rev_id)
     article=get_object_or_404(models.Reviews,pk=rev_id)
     print(article)
     context={"article":article}
+    recommend()
+    print(recommendation)
     return render(request,"WriteReview/MainPage.html",context)
-
+    
 
